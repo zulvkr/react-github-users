52530f4f41d21101e0da5d7139b29945ca2467fb
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-10 13:29:42 +0700

    add app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import Form from './components/Form'
+import CardList from './components/CardList'
 
 function App () {
+  const [cards, setCards] = useState([])
+  const addNewCard = card => {
+    setCards([...cards, card])
+  }
   return (
     <div>
       <h1 className='pt-10 mt-6 text-3xl font-extrabold leading-9 text-center text-gray-900'>
         Search a GitHub User
       </h1>
-      <Form />
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
     </div>
   )
 }
